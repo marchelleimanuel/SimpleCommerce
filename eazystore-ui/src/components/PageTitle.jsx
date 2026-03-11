7a51857cb4d069896cf95f14ad7d2569c9bbdd13
Marchelle Imanuel <marchelleimanuel@gmail.com>
2026-03-11 15:59:43 +0700

    Change CSS style using Tailwind CSS

--- eazystore-ui/src/components/PageTitle.jsx
+++ eazystore-ui/src/components/PageTitle.jsx
@@ -1,7 +1,7 @@
 
 const PageTitle = ({title}) => {
     return (
-        <h1 className="page-title">{title}</h1>
+        <h1 className="text-3xl font-primary font-extrabold text-center text-primary mt-4 py-2">{title}</h1>
     )
 }
 
--- eazystore-ui/src/components/footer/Footer.jsx
+++ eazystore-ui/src/components/footer/Footer.jsx
@@ -1,14 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import './footer.css'
 
 const Footer = () => {
     return (
-        <footer className="footer">
+        <footer className="flex justify-center items-center py-4 font-primary text-gray-700">
             Built with
-            <FontAwesomeIcon icon={faHeart} className="footer-icon" aria-hidden="true" />
+            <FontAwesomeIcon icon={faHeart} className="text-red-600 mx-1 animate-pulse" aria-hidden="true" />
             by 
-            <a href="https://eazybytes.com/" target="_blank" rel="noreferrer" >eazybytes</a>
+            <a href="https://eazybytes.com/" target="_blank" rel="noreferrer" className="text-primary font-semibold px-1 transition-colors duration-300 hover:text-dark">eazybytes</a>
         </footer>
     );
 }
