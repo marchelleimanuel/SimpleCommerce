17de8854f77bf6aca72939379dfc3b3bb00ec11b
Marchelle Imanuel <marchelleimanuel@gmail.com>
2026-03-19 22:43:15 +0700

    initialize new Product table and add Lombok plugin

--- eazystore/src/main/resources/sql/schema.sql
+++ eazystore/src/main/resources/sql/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS products
     description VARCHAR(500) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     popularity INT NOT NULL,
-    image_url VARCHAR(500) NOT NULL,
+    image_url VARCHAR(500),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     created_by VARCHAR(20) NOT NULL,
     updated_at TIMESTAMP DEFAULT NULL,
